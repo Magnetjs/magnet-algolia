27666da375ff6481cd925e8e139e82e1d7c342ce
Kieve Chua <kievechua@gmail.com>
2017-03-31 15:04:56 +0800

    Suppport auto build indexes

---
+++ dist/config/algoliasearch.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// https://github.com/algolia/algoliasearch-client-javascript
+exports.default = {
+    // Get from https://www.algolia.com/api-keys
+    applicationId: '',
+    apiKey: '',
+    // Access through app.algoliasearchIndexes.user
+    indexes: {}
+    // https://github.com/algolia/algoliasearch-client-javascript#client-options
+};
+//# sourceMappingURL=algoliasearch.js.map
--- src/config/algoliasearch.ts
+++ src/config/algoliasearch.ts
@@ -1,5 +1,13 @@
+// https://github.com/algolia/algoliasearch-client-javascript
 export default {
   // Get from https://www.algolia.com/api-keys
   applicationId: '',
-  apiKey: ''
+  apiKey: '',
+
+  // Access through app.algoliasearchIndexes.user
+  indexes: {
+    // user: 'user'
+  }
+
+  // https://github.com/algolia/algoliasearch-client-javascript#client-options
 }
